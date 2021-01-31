24fdae05d0f922ac96fb71b23f227af0536647e1
计宏华 <jihonghua@qudian.com>
2021-01-31 16:46:19 +0800

    添加启动页

---
+++ app/src/main/java/com/example/boxuegu/activity/CourseActivity.java
@@ -0,0 +1,17 @@
+package com.example.boxuegu.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.boxuegu.R;
+
+public class CourseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course);
+
+    }
+}
--- app/src/main/java/com/example/boxuegu/activity/SplashActivity.java
+++ app/src/main/java/com/example/boxuegu/activity/SplashActivity.java
@@ -2,15 +2,61 @@ package com.example.boxuegu.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
+import com.example.boxuegu.MainActivity;
 import com.example.boxuegu.R;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class SplashActivity extends AppCompatActivity {
+    TextView tv_version;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        init();
     }
+
+    private void init() {
+        tv_version = findViewById(R.id.splash_tv_version);
+        try {
+            PackageInfo info;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                info = getPackageManager().getPackageInfo(getOpPackageName(), 0);
+            } else {
+                info = getPackageManager().getPackageInfo(getPackageName(), 0);
+            }
+            if (info != null) {
+                Log.i(getLocalClassName(), "结果" + 0b110);
+                tv_version.setText(String.format(getResources().getString(R.string.splash_version), info.versionName));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            tv_version.setText("V");
+        }
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                // TODO: 是不是有直接销毁本页面的跳转方式
+                startActivity(intent);
+                // TODO: 是不是有直接销毁本页面的跳转方式
+                finish();
+            }
+        };
+        timer.schedule(task, 3000);
+    }
+
 }
